f09aa33d63bc8bc142d0ea673b8889c4cb8dd8b9
SHRIRAM7V <230121.IT@rmkec.ac.in>
2025-06-18 13:14:07 +0530

    Total monthly cost

--- client/Quantifi/src/pages/DashBoard/Dashboard.jsx
+++ client/Quantifi/src/pages/DashBoard/Dashboard.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useUser } from "@clerk/clerk-react";
 import FinancialRecordForm from "./financial-record-form";
+import { useFinancialRecordContext } from "../../context/financial-record-context";
 import FinancialRecordList from "./financial-record-list";
 const Dashboard = () => {
   const { user } = useUser();
+
+  const { records } = useFinancialRecordContext();
+  const totalmonthly = useMemo(() => {
+
+    let totalcost = 0;
+    if (!records || records.length === 0) {
+      totalcost = 0;
+    } else {
+      totalcost = records.reduce((acc, record) => acc + (record.amount || 0), 0);
+    }
+    return totalcost;
+  }, [records]);
+  
+  
   return (
     <div>
       <h1>Welcome {user?.username}! to Quantifi </h1>
       <FinancialRecordForm />
+      <div>Total Monthly cost: {totalmonthly}</div>
       <FinancialRecordList />
     </div>
   );
